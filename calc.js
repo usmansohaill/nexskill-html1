0886338c85bd3e9f3caf004949608f76c4f96ab4
Usman Sohail <usohailsheikh@yahoo.com>
2022-11-24 11:53:11 +0500

    first commit

---
+++ calc.js
@@ -0,0 +1,4 @@
+let totalMarks = prompt("Enter total marks");
+let obtainedMarks = prompt("Enter obtained marks");
+percentageObtained = (obtainedMarks / totalMarks) * 100;
+document.write(percentageObtained);
